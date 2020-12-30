4b5f7fb7ad5581c93c1ed4d921e8eb0989b82e02
Matt McKnight <mattsmcknight@gmail.com>
2020-12-30 13:29:59 -0600

    update to add additional dropboxes

--- dropbox_to_ftp.py
+++ dropbox_to_ftp.py
@@ -30,19 +30,17 @@ def idempotency(visited_files):
 
 def main():
     with open('app_token.txt', 'r') as app_file:
-        app_token = app_file.readline()
-    app_token = app_token.strip()
+        app_tokens = [ line.strip() for line in app_file.readline()]
     visited_files = idempotency(set())
-    dbx = dropbox.Dropbox(app_token)
-    #print(dbx.users_get_current_account())
-    for entry in dbx.files_list_folder('').entries:
-        visited_files.add(entry.name)
-        meta, response = dbx.files_download(entry.path_lower)
-        binary_file = BytesIO(response.content)
-        send_to_ftp(entry.name, "192.168.1.97:2221", binary_file)
-    visited_files = idempotency(visited_files)
-
-    # send_to_ftp(filename, "192.168.1.97:2221", binary_file)
+    for app_token in app_tokens
+        dbx = dropbox.Dropbox(app_token)
+        #print(dbx.users_get_current_account())
+        for entry in dbx.files_list_folder('').entries:
+            visited_files.add(entry.name)
+            meta, response = dbx.files_download(entry.path_lower)
+            binary_file = BytesIO(response.content)
+            send_to_ftp(entry.name, "192.168.1.97:2221", binary_file)
+            visited_files = idempotency(visited_files)
 
 if __name__ == "__main__":
     main()
