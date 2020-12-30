9d568d3a34ab0aa9e634ba4d5130988ca5faf83a
Matt McKnight <mattsmcknight@gmail.com>
2020-12-30 13:33:08 -0600

    fix tokens

--- dropbox_to_ftp.py
+++ dropbox_to_ftp.py
@@ -30,9 +30,11 @@ def idempotency(visited_files):
 
 def main():
     with open('app_token.txt', 'r') as app_file:
-        app_tokens = [ line.strip() for line in app_file.readline()]
+        app_tokens = [ line.strip() for line in app_file.readlines()]
     visited_files = idempotency(set())
-    for app_token in app_tokens
+    print(app_tokens)
+    for app_token in app_tokens:
+        print(app_token)
         dbx = dropbox.Dropbox(app_token)
         #print(dbx.users_get_current_account())
         for entry in dbx.files_list_folder('').entries:
